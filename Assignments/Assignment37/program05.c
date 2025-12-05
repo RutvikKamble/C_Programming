5bffb360319caf6bf2037cb566eeb05f618215cd
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-05 15:17:51 +0530

    program05 update

---
+++ Assignments/Assignment37/program05.c
@@ -0,0 +1,33 @@
+// WAP whic accept string from user and reverst that string in place  
+
+#include<stdio.h>
+
+void ReverseDisplay(char *str)
+{
+    char *temp = str;
+
+    while(*str != '\0')
+    {
+        str++;
+    }
+
+    while(str >= temp)
+    {
+        printf("%c",*str);
+        str--;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",&Arr);
+
+    ReverseDisplay(Arr);
+
+    return 0;
+}
