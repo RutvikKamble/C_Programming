21dda62fe3e75a89beee87d5286f6a32f046fd6a
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-23 11:38:14 +0530

    program03

---
+++ Assignments/Assignment43/program03.c
@@ -0,0 +1,109 @@
+// WAP which display all even elements from singly linear linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (struct node *)malloc(sizeof(struct node));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+void DisplayAddEven(PNODE first)
+{
+    printf("The even number frmo the linked list : ");
+    while(first != NULL)
+    {
+        if(first->data%2 == 0)
+        {
+            printf("%d\t",first->data);
+        }
+         first = first->next;
+    }
+}
+
+int main()
+{
+    int iCnt = 0, iValue = 0, iNo = 0, iRet = 0;
+    
+    PNODE head = NULL;
+
+    printf("Enter the number for node : ");
+    scanf("%d",&iValue);
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        printf("Insert the data for node : ");
+        scanf("%d",&iNo);
+
+        InsertLast(&head,iNo);
+    }
+
+    Display(head);
+
+    DisplayAddEven(head);
+
+
+    return 0;
+}
