37627ad7548da690c7d79a7b00e6644fb742d5ac
Rutvik Kamble <rutvikkamble016@gmail.com>
2026-01-02 18:33:56 +0530

    Add files via upload
    program01 / program02/ program03 / program04 / program05

---
+++ Assignments/Assignment46/program03.c
@@ -0,0 +1,191 @@
+//  Display numbers greter than X
+
+/*
+    Algorithm
+        Start
+            Accept the N values from user
+            Accept another number
+            Check how many numbers are greter than the X 
+        Stop
+*/
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Required Header file                                                       //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+// Typedef
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int Data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : InsertFirst                                                //
+//  Description :   Insert the value on the first of LL                        //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       30/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(struct node **first, int no)
+{
+    struct node *newn = NULL;
+
+    newn = (struct node *)malloc(sizeof(struct node));
+
+    newn->Data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : InsertLast                                                 //
+//  Description :   Insert the value on last of the LL                         //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       30/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : Display                                                    //
+//  Description :   Display the all emement from LL                            //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       30/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void Display(struct node *first)
+{
+    while(first != NULL)
+    {
+        printf("|%d|->",first->Data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : Count                                                      //
+//  Description :   Count the node from the ll                                 //
+//  Input   :       int                                                        //
+//  Output  :       bool                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       30/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+bool Count(PNODE first)
+{
+    int iCount = 0;
+    while (first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+    if (iCount == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Entry point functon of the program                                         //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0,iCnt = 0, iNo1 = 0;
+    bool bRet = false;
+
+    struct node *head = NULL;
+
+    printf("Enter the number for node : ");
+    scanf("%d",&iValue);
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        printf("Enter teh data for node : ");
+        scanf("%d",&iNo1);
+
+        InsertLast(&head,iNo1);
+    }
+
+    Display(head);
+    bRet = Count(head);
+
+    if (bRet == true)
+    {
+        printf("The linked list is empty.");
+    }
+    else
+    {
+        printf("The linked contain one or more than one emement.");
+    }
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// End of main                                                                 //
+/////////////////////////////////////////////////////////////////////////////////
