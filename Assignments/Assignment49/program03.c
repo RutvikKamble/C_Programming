efe5b531c49da01ca1c57b61cc997cdf5c378967
Rutvik008-co <rutvikkamble016@gmail.com>
2026-01-05 13:04:42 +0530

    Assignment49

---
+++ Assignments/Assignment49/program03.c
@@ -0,0 +1,186 @@
+// Display Element Divisible by 3
+
+/*
+    Algorithm
+        Start
+            Accept the N values from user
+            and 
+            Check it divisible by 3 or not. 
+        Stop
+*/
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Required Header file                                                       //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// Define
+#define TRUE 1
+#define FALSE 0
+
+// Typedef
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : InsertFirst                                                //
+//  Description :   Insert the value on the first of LL                        //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       31/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(struct node **first, int no)
+{
+    struct node *newn = NULL;
+
+    newn = (struct node *)malloc(sizeof(struct node));
+
+    newn->Data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : InsertLast                                                 //
+//  Description :   Insert the value on last of the LL                         //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       31/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : Display                                                    //
+//  Description :   Display the all emement from LL                            //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       31/12/2025                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void Display(struct node *first)
+{
+    while(first != NULL)
+    {
+        printf("|%d|->",first->Data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Function Name : DisplayDivByThree                                          //
+//  Description :   Display all element which Divisible by 3                   //
+//  Input   :       int                                                        //
+//  Output  :       void                                                       //
+//  Author  :       Rutvik Bibhishan Kamble                                    //
+//  Date    :       04/01/2026                                                 //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+void DisplayDivByThree(PNODE first)
+{
+    PNODE temp = NULL;
+    int Count = 0;
+
+    temp = first;
+
+    printf("The numbers which are Divisible by 3 from LL : \t\t");
+    while (temp != NULL)
+    {
+        if(temp->Data % 3 == 0)
+        {
+            printf("%d\t",temp->Data);
+        }
+        temp = temp->next;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//  Entry point functon of the program                                         //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0,iCnt = 0, iNo1 = 0, iRet = 0;
+
+    struct node *head = NULL;
+
+    printf("Enter the number for node : ");
+    scanf("%d",&iValue);
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        printf("Enter the data for node : ");
+        scanf("%d",&iNo1);
+
+        InsertLast(&head,iNo1);
+    }
+
+    Display(head);
+    DisplayDivByThree(head);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// End of main                                                                 //
+/////////////////////////////////////////////////////////////////////////////////
